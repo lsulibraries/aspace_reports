b2614ac5a84a1db137a7b4650900a41b938d092b
Jason Peak <jpeak5@lsu.edu>
2016-06-06 14:20:51 -0500

    all together now

--- accession-record.sql
+++ accession-record.sql
@@ -47,6 +47,7 @@ select
 from extent e
 where e.accession_id = 95;
 
+
 /*
 +--------+-------+---------+
 | Number | Type  | Portion |
@@ -113,3 +114,47 @@ where elr.accession_id = 95;
 | cataloged  | jmitc84    | 2015-11-18 20:27:18 | jmitc84          | 2015-11-18 20:27:18 |
 +------------+------------+---------------------+------------------+---------------------+
 */
+
+
+--- ALL TOGETHER NOW...
+
+select 
+      acc.title Title, 
+      acc.identifier, 
+      acc.content_description "Content Description", 
+      acc.accession_date "Accession Date",
+      ( SELECT value FROM enumeration_value where id = acc.acquisition_type_id) "Acquisition Type", 
+      ( SELECT value FROM enumeration_value where id = acc.resource_type_id) "Resource Type", 
+      acc.restrictions_apply "Restrictions Apply", 
+      acc.publish "Publish", 
+      acc.access_restrictions "Access Restrictions", 
+      acc.use_restrictions "Use Restrictions",
+      ud.string_1 "Mss Number", 
+      ud.string_2 "Location", 
+      ud.string_3 "SIRSI Number",
+      ud.date_1 "Date Received",
+      e.number Number, 
+      (select value from enumeration_value where id = e.extent_type_id) Type,
+      (select value from enumeration_value where id = e.portion_id)  Portion,
+      np.sort_name agent, 
+      ac.address_1, 
+      ac.city, 
+      ac.region, 
+      ac.post_code,
+      (select value from enumeration_value where id = lar.role_id) role,
+      (select ev.value from enumeration_value ev where ev.id = evt.event_type_id ) event_type, 
+      evt.created_by "Created By", 
+      evt.create_time "Date Created", 
+      evt.last_modified_by "Last Modified By", 
+      evt.user_mtime "Date modified"
+
+from accession acc 
+inner join 
+      user_defined ud on ud.accession_id = acc.id
+      join extent e on e.accession_id = acc.id
+      join linked_agents_rlshp lar on lar.accession_id = acc.id
+      join name_person np on lar.agent_person_id = np.id 
+      join agent_contact ac on lar.agent_person_id = ac.agent_person_id 
+      join event_link_rlshp elr on elr.accession_id = acc.id
+      join event evt on evt.id = elr.event_id 
+where acc.id = 95\G
